309fd1a81987f14f3b03f6ff68eb0d8f671a6d2b
Remi Dettai <rdettai@gmail.com>
2023-01-11 10:01:56 +0000

    Rewrite script with plain docker instead of compose

--- scripts/regression-tests.sh
+++ scripts/regression-tests.sh
@@ -1,36 +1,51 @@
 #!/usr/bin/env bash
 
+# The tag on docker.io the database is created with
 OLD_VERSION=$1
+# A tag on ghcr.io to validate retro compatibility
 NEW_VERSION=$2
 
 set -euxo pipefail
 
-pushd "$(git -C "$(dirname "$(readlink -f "${0}")")" rev-parse --show-toplevel)/docker/compose"
+pushd "$(git -C "$(dirname "$(readlink -f "${0}")")" rev-parse --show-toplevel)"
+
+VAST_RUN_FLAGS="--pull --rm --name vast-regression -e VAST_CONSOLE_VERBOSITY=verbose -v vast-regression:/var/lib/vast/"
 
 # Pull the old version to create a database.
-export COMPOSE_FILE="vast.yaml:vast.volume.yaml"
-export VAST_CONSOLE_VERBOSITY=verbose
-export VAST_CONTAINER_REF=$OLD_VERSION
-export VAST_CONTAINER_REGISTRY=docker.io
-docker compose pull
-docker compose up --detach --force-recreate vast
-sleep 5
-docker compose logs
-docker compose run --rm --no-TTY --entrypoint bash vast -c \
-    "vast import --blocking suricata && vast flush" \
-    < ../../vast/integration/data/suricata/eve.json
-docker compose run --rm --interactive=false vast export json \
-    '#type == "suricata.alert"' > old.json
-docker compose down
-sleep 5
-# Pull the new version to double-check the output.
-export VAST_CONTAINER_REF=$NEW_VERSION
-export VAST_CONTAINER_REGISTRY=ghcr.io
-docker compose pull
-docker compose up --detach --force-recreate vast
-sleep 5
-docker compose run --rm --interactive=false vast export json \
-  '#type == "suricata.alert"' > new.json
-docker compose down -v
+docker run \
+  $VAST_RUN_FLAGS \
+  docker.io/tenzir/vast:$OLD_VERSION \
+  start &
+
+sleep 3
+
+docker exec -i vast-regression \
+  vast import --blocking suricata \
+  < vast/integration/data/suricata/eve.json
+
+docker exec vast-regression \
+  vast flush
+
+docker exec vast-regression \
+  vast export json '#type == "suricata.alert"' \
+  > old.json
+
+docker rm -f vast-regression
+
+# Pull the new version to verify database compatibility.
+docker run \
+  $VAST_RUN_FLAGS \
+  ghcr.io/tenzir/vast:$NEW_VERSION \
+  start &
+
+sleep 3
+
+docker exec vast-regression \
+  vast export json '#type == "suricata.alert"' \
+  > new.json
+
+docker rm -f vast-regression
+docker volume rm vast-regression
+
 # Compare old and new
 diff old.json new.json
